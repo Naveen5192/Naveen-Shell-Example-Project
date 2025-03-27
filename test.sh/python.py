1ac64dfe857d6e69e3eb1596116fdff1bb490685
Chowdary <nandamuri-naveen.chowdary@capgemini.com>
2025-03-27 19:38:30 +0530

    test2

---
+++ test.sh/python.py
@@ -0,0 +1,16 @@
+# This script defines a function that calculates the area of a rectangle
+
+def calculate_area(length, width):
+    """Calculate the area of a rectangle."""
+    return length * width
+
+# Define dimensions
+length = 7
+width = 3
+
+# Calculate area
+area = calculate_area(length, width)
+
+# Print result
+print(f"The area of the rectangle is: {area} square units")
+
